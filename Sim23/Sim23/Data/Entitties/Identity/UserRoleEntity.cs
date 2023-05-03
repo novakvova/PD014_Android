da894b51d2bc1add3129acbdf34702e9a4091525
novakvova <novakvova@gmail>
2023-05-03 15:40:58 +0300

    Add Identity tabels

---
+++ Sim23/Sim23/Data/Entitties/Identity/UserRoleEntity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Sim23.Data.Entitties.Identity
+{
+    public class UserRoleEntity : IdentityUserRole<int>
+    {
+        public virtual UserEntity User { get; set; }
+        public virtual RoleEntity Role { get; set; }
+    }
+}
